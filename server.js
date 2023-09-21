539605ae1ea249090e6d313b4e8c95340401210f
MD.AL-AMIN <101187208+alaminAppnap@users.noreply.github.com>
2023-09-21 14:25:24 +0600

    redis updated config

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express()
 const port = 3000
 
 const Redis = require("ioredis");
-const redis = new Redis();
-const subscriber = new Redis();
+const redis = new Redis({
+  host: 'localhost', // Replace with your Redis server host
+  port: 6379,        // Replace with your Redis server port
+  password: 'your_custom_password', // Replace with your custom Redis password
+});
+
+const subscriber = new Redis({
+  host: 'localhost', // Replace with your Redis server host
+  port: 6379,        // Replace with your Redis server port
+  password: 'your_custom_password', // Replace with your custom Redis password
+});
 
 const channelName = 'laravelsmssend'; 
 subscriber.subscribe(channelName);
