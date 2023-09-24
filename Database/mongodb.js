66035565ed147e01748f5471604462d298ecbfbf
alaminAppnap <alamin@appnap.io>
2023-09-24 15:59:40 +0600

    mongo configure update

--- Database/mongodb.js
+++ Database/mongodb.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-// const dbUrl = 'mongodb://localhost:27017/your-database-name';
-const dbUrl = 'mongodb+srv://alamin:alamin899@cluster0.pifb15t.mongodb.net/?retryWrites=true&w=majority';
+const dbUrl = 'mongodb://localhost:27017/your-database-name';
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express')
-const axios = require('axios');
 const app = express()
 require('dotenv').config();
-const {db} = require('./Database/mongodb');
+const {db} = require('./Database/mongodb')
 
 const { sendNotification } = require('./Notifications/slackNotificationService');
 const { sendSslMessage } = require('./SMS/sslWirelessSMSProvider');
