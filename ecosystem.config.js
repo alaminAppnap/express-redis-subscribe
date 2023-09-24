43bd036885f07eef413ffd57ef053659fa4967ea
alaminAppnap <alamin@appnap.io>
2023-09-24 18:45:23 +0600

    pm2 watch alawys

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         script: 'server.js',
         instances: 1,
         autorestart: true,
-        watch: false,
+        watch: true,
         max_memory_restart: '1G',
       },
     ],
